8d12eae3dd8b5ed6771cf41d6f2650d8df85500c
Danish Ali <118071540+danish0501@users.noreply.github.com>
2023-11-18 19:48:04 +0530

    Add files via upload

---
+++ build/precache-manifest.ca22e2b8fdbd9f09f7be6b607f4b0b9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0de9e0664453e23b0bd0",
+    "url": "/static/css/main.865ec2b7.chunk.css"
+  },
+  {
+    "revision": "0de9e0664453e23b0bd0",
+    "url": "/static/js/main.0de9e066.chunk.js"
+  },
+  {
+    "revision": "537741e50dfa0f444ed8",
+    "url": "/static/js/1.537741e5.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "49e2ec96689dfe299f1b0749e2b1085e",
+    "url": "/index.html"
+  }
+];
